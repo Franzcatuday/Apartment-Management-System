9837e4ef7d4cf7c802a50f7e5e6632e4fe2a9446
Franzcatuday <118037700+Franzcatuday@users.noreply.github.com>
2024-02-11 00:20:50 +0800

    Add files via upload

---
+++ admin/apartmentedit.php
@@ -0,0 +1,361 @@
+<?php
+session_start();
+require("config.php");
+
+ 
+if(!isset($_SESSION['auser']))
+{
+	header("location:index.php");
+}
+
+
+$error="";
+$msg="";
+if(isset($_POST['add']))
+{
+	$pid=$_REQUEST['id'];
+	
+	$title=$_POST['title'];
+	$content=$_POST['content'];
+	$ptype=$_POST['ptype'];
+	$bed=$_POST['bed'];
+	$stype=$_POST['stype'];
+	$bath=$_POST['bath'];
+	$kitc=$_POST['kitc'];
+	$floor=$_POST['floor'];
+	$price=$_POST['price'];
+	$city=$_POST['city'];
+	$loc=$_POST['loc'];
+	$state=$_POST['state'];
+	$status=$_POST['status'];
+	$feature=$_POST['feature'];
+	
+
+	
+	$aimage=$_FILES['aimage']['name'];
+	$aimage1=$_FILES['aimage1']['name'];
+	$aimage2=$_FILES['aimage2']['name'];
+	$aimage3=$_FILES['aimage3']['name'];
+	$aimage4=$_FILES['aimage4']['name'];
+	
+	
+	
+	$temp_name  =$_FILES['aimage']['tmp_name'];
+	$temp_name1 =$_FILES['aimage1']['tmp_name'];
+	$temp_name2 =$_FILES['aimage2']['tmp_name'];
+	$temp_name3 =$_FILES['aimage3']['tmp_name'];
+	$temp_name4 =$_FILES['aimage4']['tmp_name'];
+	
+	
+	
+	move_uploaded_file($temp_name,"property/$aimage");
+	move_uploaded_file($temp_name1,"property/$aimage1");
+	move_uploaded_file($temp_name2,"property/$aimage2");
+	move_uploaded_file($temp_name3,"property/$aimage3");
+	move_uploaded_file($temp_name4,"property/$aimage4");
+	
+	
+	
+	$sql = "UPDATE property SET title= '{$title}', pcontent= '{$content}', type='{$ptype}',bedroom='{$bed}', stype='{$stype}',
+    bathroom='{$bath}',  kitchen='{$kitc}', floor='{$floor}', 
+	 price='{$price}',city='{$city}', location='{$loc}', state='{$state}', status='{$status}', feature='{$feature}',
+	pimage='{$aimage}', pimage1='{$aimage1}', pimage2='{$aimage2}', pimage3='{$aimage3}', pimage4='{$aimage4}',
+	 status='{$status}'	
+	 WHERE pid = {$pid}";
+	
+	$result=mysqli_query($con,$sql);
+	if($result == true)
+	{
+		$msg="<p class='alert alert-success'>Apartment Updated</p>";
+		header("Location:apartmentview.php?msg=$msg");
+	}
+	else{
+		$msg="<p class='alert alert-warning'>Apartment Not Updated</p>";
+		header("Location:apartmentview.php?msg=$msg");
+	}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Apartment</title>
+		
+        <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+		
+		
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		
+		
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		
+		
+        <link rel="stylesheet" href="assets/css/feathericon.min.css">
+		
+		
+        <link rel="stylesheet" href="assets/css/style.css">
+		
+		
+    </head>
+    <body>
+
+		
+			
+			<?php include("header.php"); ?>
+			
+            <div class="page-wrapper">
+                <div class="content container-fluid">
+				
+					
+					<div class="page-header">
+						<div class="row">
+							<div class="col">
+								<h3 class="page-title">Apartment</h3>
+								<ul class="breadcrumb">
+									<li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+									<li class="breadcrumb-item active">Apartment</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				
+					
+					<div class="row">
+						<div class="col-md-12">
+							<div class="card">
+								<div class="card-header">
+									<h4 class="card-title">Update Apartment Details</h4>
+									<?php echo $error; ?>
+									<?php echo $msg; ?>
+								</div>
+								<form method="post" enctype="multipart/form-data">
+								
+								<?php
+									
+									$pid=$_REQUEST['id'];
+									$query=mysqli_query($con,"select * from property where pid='$pid'");
+									while($row=mysqli_fetch_row($query))
+									{
+								?>
+												
+								<div class="card-body">
+									<h5 class="card-title">Apartment Detail</h5>
+										<div class="row">
+											<div class="col-xl-12">
+												<div class="form-group row">
+													<label class="col-lg-2 col-form-label">Title</label>
+													<div class="col-lg-9">
+														<input type="text" class="form-control" name="title" required value="<?php echo $row['1']; ?>">
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-lg-2 col-form-label">Content</label>
+													<div class="col-lg-9">
+														<textarea class="tinymce form-control" name="content" rows="10" cols="30"><?php echo $row['2']; ?></textarea>
+													</div>
+												</div>
+												
+											</div>
+											<div class="col-xl-6">
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">Apartment Type</label>
+													<div class="col-lg-9">
+														<select class="form-control" required name="ptype">
+															<option value="">Select Type</option>
+															<option value="apartment">Apartment</option>
+															<option value="flat">Flat</option>
+															<option value="building">Building</option>
+															<option value="house">House</option>
+															<option value="villa">Villa</option>
+															<option value="office">Office</option>
+														</select>
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">Selling Type</label>
+													<div class="col-lg-9">
+														<select class="form-control" required name="stype">
+															<option value="">Select Status</option>
+															<option value="rent">Rent</option>
+															<option value="sale">Sale</option>
+														</select>
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">Bathroom</label>
+													<div class="col-lg-9">
+														<input type="text" class="form-control" name="bath" required value="<?php echo $row['6']; ?>">
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">Kitchen</label>
+													<div class="col-lg-9">
+														<input type="text" class="form-control" name="kitc" required value="<?php echo $row['7']; ?>">
+													</div>
+												</div>
+											
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">Bedroom</label>
+													<div class="col-lg-9">
+														<input type="text" class="form-control" name="bed" required value="<?php echo $row['5']; ?>">
+													</div>
+												</div>
+												
+												
+											</div>
+										</div>
+										<h4 class="card-title">Price & Location</h4>
+										<div class="row">
+											<div class="col-xl-6">
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">Floor</label>
+													<div class="col-lg-9">
+														<select class="form-control" required name="floor">
+															<option value="">Select Floor</option>
+															<option value="1st Floor">1st Floor</option>
+															<option value="2nd Floor">2nd Floor</option>
+															<option value="3rd Floor">3rd Floor</option>
+															<option value="4th Floor">4th Floor</option>
+															<option value="5th Floor">5th Floor</option>
+														</select>
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">Price</label>
+													<div class="col-lg-9">
+														<input type="text" class="form-control" name="price" required value="<?php echo $row['9']; ?>">
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">City</label>
+													<div class="col-lg-9">
+														<input type="text" class="form-control" name="city" required value="<?php echo $row['11']; ?>">
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">State</label>
+													<div class="col-lg-9">
+														<input type="text" class="form-control" name="state" required value="<?php echo $row['12']; ?>">
+													</div>
+												</div>
+											</div>
+											<div class="col-xl-6">
+												
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">Address</label>
+													<div class="col-lg-9">
+														<input type="text" class="form-control" name="loc" required value="<?php echo $row['10']; ?>">
+													</div>
+												</div>
+												
+											</div>
+										</div>
+										
+										<div class="form-group row">
+											<label class="col-lg-2 col-form-label">Feature</label>
+											<div class="col-lg-9">
+											
+											<textarea class="tinymce form-control" name="feature" rows="10" cols="30">
+												
+													<?php echo $row['13']; ?>
+												
+											</textarea>
+											</div>
+										</div>
+												
+										<h4 class="card-title">Image & Status</h4>
+										<div class="row">
+											<div class="col-xl-6">
+												
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">Image</label>
+													<div class="col-lg-9">
+														<input class="form-control" name="aimage" type="file" required="">
+														<img src="property/<?php echo $row['14'];?>" alt="pimage" height="150" width="180">
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">Image 2</label>
+													<div class="col-lg-9">
+														<input class="form-control" name="aimage2" type="file" required="">
+														<img src="property/<?php echo $row['16'];?>" alt="pimage" height="150" width="180">
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">Image 4</label>
+													<div class="col-lg-9">
+														<input class="form-control" name="aimage4" type="file" required="">
+														<img src="property/<?php echo $row['18'];?>" alt="pimage" height="150" width="180">
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">Status</label>
+													<div class="col-lg-9">
+														<select class="form-control"  required name="status">
+															<option value="">Select Status</option>
+															<option value="available">Available</option>
+															<option value="sold out">Not Available</option>
+														</select>
+													</div>
+												</div>
+												
+											</div>
+											<div class="col-xl-6">
+												
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">Image 1</label>
+													<div class="col-lg-9">
+														<input class="form-control" name="aimage1" type="file" required="">
+														<img src="property/<?php echo $row['15'];?>" alt="pimage" height="150" width="180">
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-lg-3 col-form-label">image 3</label>
+													<div class="col-lg-9">
+														<input class="form-control" name="aimage3" type="file" required="">
+														<img src="property/<?php echo $row['17'];?>" alt="pimage" height="150" width="180">
+													</div>
+												</div>
+												
+												
+												
+											</div>
+										</div>
+
+										<hr>
+
+										
+
+										
+											<input type="submit" value="Submit" class="btn btn-primary"name="add" style="margin-left:200px;">
+										
+									</div>
+								</form>
+								
+								<?php
+									} 
+								?>
+												
+							</div>
+						</div>
+					</div>
+				
+				</div>			
+			</div>
+		
+        <script src="assets/js/jquery-3.2.1.min.js"></script>
+		<script src="assets/plugins/tinymce/tinymce.min.js"></script>
+		<script src="assets/plugins/tinymce/init-tinymce.min.js"></script>
+		
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+		
+        <script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+		
+		
+		<script  src="assets/js/script.js"></script>
+		
+    </body>
+
+</html>
